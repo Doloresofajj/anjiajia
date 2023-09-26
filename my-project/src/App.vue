2f966eeeed3e70efe985a57a83c905e43afd2ebc
anjiajia <2844654853@qq.com>
2023-09-26 13:10:03 +0800

    first commit

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,26 +1,36 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<template>  
+  <div id="app">  
+    <img alt="Vue logo" src="./assets/1111.png" class="avatar">  
+    <div class="info">    
+      <h1 class="nickname">Hi,I'm Dolores</h1>    
+      <h2 class="student-id">学号：2310273045，联系方式：AJJ8560538@gmail.com</h2>  
+      <h3 class="want">生有热烈，藏于俗常</h3>  
+    </div>  
+  </div>  
+</template>  
+  
+  
+<style>  
+#app {  
+  font-family: Avenir, Helvetica, Arial, sans-serif;  
+  -webkit-font-smoothing: antialiased;  
+  -moz-osx-font-smoothing: grayscale;  
+  text-align: center;  
+  color: #2c3e50;  
+  margin-top: 60px;  
+}  
+  
+.avatar {  
+  border-radius: 50%; /* 这行代码会将头像设为圆形 */  
+}  
+.student-id {
+  font-size:medium;
+  font-weight: lighter;
+  color: darkslategray;
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.want {
+  font-size:medium;
+  font-weight: lighter;
+  color: darkslategray;
 }
 </style>
